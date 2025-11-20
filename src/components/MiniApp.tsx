57cf8b85d5403e4936ab6af1bad0d78d1d751a33
raptor0929 <flaura.dev@gmail.com>
2025-11-20 12:26:40 -0300

    comment approve

--- src/components/MiniApp.tsx
+++ src/components/MiniApp.tsx
@@ -60,17 +60,17 @@ export const MiniApp: React.FC = () => {
     try {
       const batchResult = await callSmartContract({
         contracts:[
-          {
-            contractAddress: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
-            functionName: "approve",
-            functionParams: ["0x644F71d3376b44965222829E6974Ad88459b608D", amount],
-            value: "0",
-            chainId: ChainId.BASE_SEPOLIA
-          },
+          // {
+          //   contractAddress: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
+          //   functionName: "approve",
+          //   functionParams: ["0x644F71d3376b44965222829E6974Ad88459b608D", amount],
+          //   value: "0",
+          //   chainId: ChainId.BASE_SEPOLIA
+          // },
           {
             contractAddress: "0x644F71d3376b44965222829E6974Ad88459b608D",
             functionName: "deposit",
-            functionParams: ["0x036CbD53842c5426634e7929541eC2318f3dCF7e", amount, "604800", deadline, "0x"],
+            functionParams: ["0x036CbD53842c5426634e7929541eC2318f3dCF7e", amount, 604800, deadline, "0x"],
             value: "0",
             chainId: ChainId.BASE_SEPOLIA
           }
